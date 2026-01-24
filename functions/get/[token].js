2c88d0db650eba024ccff9cf172ebce9d7a8a826
chikaylov <denischikaylov@gmail.com>
2026-01-24 22:31:30 +0300

    Update [token].js

--- functions/get/[token].js
+++ functions/get/[token].js
@@ -8,7 +8,7 @@ export async function onRequest({ env, params }) {
   if (status !== "active") return new Response("Access denied", { status: 403 });
 
   // достаём файл с GitHub
-  const githubUrl = "https://github.com/chikaylov/sdf89gsd78/blob/main/sdfs65fhfg056jf";
+  const githubUrl = "https://raw.githubusercontent.com/chikaylov/sdf89gsd78/refs/heads/main/sdfs65fhfg056jfv";
   const resp = await fetch(githubUrl);
   if (!resp.ok) return new Response("Failed to fetch keys", { status: 502 });
 
