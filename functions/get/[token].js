ad2674b2afc41a291879761b0bcdd39fc00942ee
chikaylov <denischikaylov@gmail.com>
2026-01-24 22:01:30 +0300

    Update [token].js

--- functions/get/[token].js
+++ functions/get/[token].js
@@ -1,10 +1,11 @@
 export async function onRequest({ env, params }) {
   const token = params.token;
 
-  if (!token) {
-    return new Response("No token provided", { status: 400 });
-  }
+  if (!token) return new Response("No token provided", { status: 400 });
 
-  // на этом этапе просто возвращаем токен для теста
-  return new Response(`Token received: ${token}`);
+  // проверяем токен в KV
+  const status = await env.USERS.get(token);
+  if (status !== "active") return new Response("Access denied", { status: 403 });
+
+  return new Response("Access granted");
 }
